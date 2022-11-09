e675d69d958139f24ab56c4cb0fa2bdca2f2d2f3
PR0PH3CY <34958544+PR0PH3CY33@users.noreply.github.com>
2022-11-09 12:19:06 +0200

    Add files via upload

---
+++ fileCreator.c
@@ -0,0 +1,32 @@
+
+#include <stdio.h>
+
+#include <sys/types.h>
+
+#include <sys/stat.h>
+
+#include <fcntl.h>
+
+#include <unistd.h>
+
+
+
+
+
+int main(int argc, char * argv[]) {
+
+	int fileCreationResult = open(argv[1], O_CREAT, 0644);
+
+	if(fileCreationResult > 0) {
+
+		printf("%s File Created\n", argv[1]);
+
+	}
+
+	else {
+
+		perror("%s Creation Failed!");
+
+	}
+
+}
